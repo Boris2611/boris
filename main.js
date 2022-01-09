46b04cf5b0740b9371cb025d2cf09a1ba8f5cba4
Boris2611 <84404796+Boris2611@users.noreply.github.com>
2022-01-09 15:55:24 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -3,13 +3,12 @@
 
 // Glow effect 2 seconds after loading site
 
-setTimeout(change2, 2000);
+setTimeout(glow, 2000);
 
-function change2() {
+function glow() {
     document.querySelector("body").style.color = "#999";
     document.getElementById("aboutme").style.border = "2px solid white";
     document.getElementById("aboutme").style.boxShadow = "0px 0px 10px 10px #333";
-
     document.getElementById("aboutme2").style.border = "2px solid white";
     document.getElementById("aboutme2").style.boxShadow = "0px 0px 10px 10px #333";
 }
